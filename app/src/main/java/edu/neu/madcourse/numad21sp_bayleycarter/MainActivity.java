11259c0c9a242a621f263c921b89bab7c230aa9e
Bayley Carter <carter.ba@northeastern.edu>
2021-02-15 17:51:24 -0500

    added third activity and link collector button

--- app/src/main/java/edu/neu/madcourse/numad21sp_bayleycarter/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21sp_bayleycarter/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     TextView txt;
     Button btn;
     Button clickHere;
+    Button linkList;
 
 //    text button
     @Override
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         txt = (TextView) findViewById(R.id.nameemail);
         btn = (Button) findViewById(R.id.aboutbutton);
         clickHere = (Button) findViewById(R.id.clickHereButton);
+        linkList = (Button) findViewById(R.id.linkButton);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,12 +40,25 @@ public class MainActivity extends AppCompatActivity {
                 getSixButtons();
             }
         });
+        linkList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLinkList();
+            }
+        });
 
     }
 
     private void getSixButtons() {
-        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-        MainActivity.this.startActivity(intent);
+        Intent intent2 = new Intent(MainActivity.this, MainActivity2.class);
+        MainActivity.this.startActivity(intent2);
+
+
+    }
+
+    private void getLinkList() {
+        Intent intent3 = new Intent(MainActivity.this, MainActivity3.class);
+        MainActivity.this.startActivity(intent3);
 
 
     }
---
+++ app/src/main/java/edu/neu/madcourse/numad21sp_bayleycarter/MainActivity3.java
@@ -0,0 +1,16 @@
+package edu.neu.madcourse.numad21sp_bayleycarter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        Intent myIntent = getIntent();
+    }
+}
