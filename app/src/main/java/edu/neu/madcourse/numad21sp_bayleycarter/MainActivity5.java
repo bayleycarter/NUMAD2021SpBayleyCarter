8c2a1d85dd907864edd7917e13824948ad1b1465
Bayley Carter <carter.ba@northeastern.edu>
2021-03-09 14:01:19 -0500

    cleaned up UI

--- app/src/main/java/edu/neu/madcourse/numad21sp_bayleycarter/MainActivity5.java
+++ app/src/main/java/edu/neu/madcourse/numad21sp_bayleycarter/MainActivity5.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.lang.Thread;
 
 import androidx.appcompat.app.AppCompatActivity;
 
